a808922f731ee17fcca071f7d6d90af760f30d62
estebancacavelos <estebancacavelos@23378979-bf50-a3ec-246b-1660a417669b>
2012-08-20 16:54:11 +0000

    Estructura inicial del proyecto.
    Pantalla principal con un toolbar y 2 botones en el toolbar. Solo va a tener 2 funcionalidades
    
    1. Disenhar el cheque. 
    2. Imprimir el cheque.

---
+++ ChqPrint/MoveThumb.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace ChqPrint
+{
+    public class MoveThumb : Thumb
+    {
+        public MoveThumb()
+        {
+            DragDelta += new DragDeltaEventHandler(this.MoveThumb_DragDelta);
+        }
+
+        private void MoveThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            Control designerItem = this.DataContext as Control;
+
+            if (designerItem != null)
+            {
+                double left = Canvas.GetLeft(designerItem);
+                double top = Canvas.GetTop(designerItem);
+                
+
+                // Se establecen los limites horizontales, no puede ser menor a 150 ni mayor a 180. 
+                if (left >= 150)
+                    Canvas.SetLeft(designerItem, left + e.HorizontalChange);
+                else
+                    Canvas.SetLeft(designerItem, 150);
+
+                    Canvas.SetTop(designerItem, top + e.VerticalChange);
+            }
+        }
+    }
+}
